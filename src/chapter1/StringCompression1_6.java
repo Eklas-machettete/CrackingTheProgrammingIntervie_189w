846ed84b0bb6a527f49527be32f5a92f218b6b18
Eklas-machettete <79093688+Eklas-machettete@users.noreply.github.com>
2022-04-19 11:01:32 +0600

    --

---
+++ src/chapter1/StringCompression1_6.java
@@ -0,0 +1,37 @@
+package chapter1;
+
+import java.util.Scanner;
+
+public class StringCompression1_6 {
+
+//	public static void main(String[] args) {
+//
+//		Scanner sc = new java.util.Scanner(System.in);
+//		String str = sc.nextLine();
+//		System.out.println(stringCompression(str));
+//
+//	}
+
+	private static StringBuilder stringCompression(String str) {
+
+		Integer count = 1;
+		StringBuilder compressed = new StringBuilder("");
+
+		char prev = str.charAt(0);
+		for (int i = 1; i < str.length(); i++) {
+			if (str.charAt(i) == prev) {
+				count++;
+			} else {
+				compressed.append(prev + count.toString());
+				prev = str.charAt(i);
+				count = 1;
+			}
+          }
+		compressed.append(prev + count.toString());
+		if (compressed.length() > str.length())
+			return new StringBuilder(str);
+		return compressed;
+
+	}
+
+}
